cf51508687652f040be5b649f0661e53cfd08c18
selfpacelearner <selfpacelearner@gmail.com>
2023-09-02 21:58:47 +0530

    sidebar added

---
+++ spl-js/spl-js.js
@@ -0,0 +1,13 @@
+
+// menu side bar
+function openNav() {
+	document.getElementById("sidebar").style.width = "200px";
+	document.getElementById("menu").style.marginLeft = "195px";
+	document.getElementById("pagecontent").style.marginLeft = "195px";
+}
+
+function closeNav() {
+	document.getElementById("sidebar").style.width = "0px";
+	document.getElementById("menu").style.marginLeft= "0px";
+	document.getElementById("pagecontent").style.marginLeft = "0px";
+}
