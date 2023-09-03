141127f7623b797b936e515278ff921ac6dca258
selfpacelearner <selfpacelearner@gmail.com>
2023-09-03 11:32:58 +0530

    worked on main page

--- spl-js/spl-js.js
+++ spl-js/spl-js.js
@@ -3,11 +3,9 @@
 function openNav() {
 	document.getElementById("sidebar").style.width = "200px";
 	document.getElementById("menu").style.marginLeft = "195px";
-	document.getElementById("pagecontent").style.marginLeft = "195px";
 }
 
 function closeNav() {
 	document.getElementById("sidebar").style.width = "0px";
 	document.getElementById("menu").style.marginLeft= "0px";
-	document.getElementById("pagecontent").style.marginLeft = "0px";
 }
